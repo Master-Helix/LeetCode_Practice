bd7756a11b7007c107931f02f2430115ac32fb21
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-03-21 18:13:43 +0530

    Time: 3 ms (85.70%), Space: 6.6 MB (64.08%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        
+        int arr[256]={0};
+        for(int i=0;i<s.size();i++)
+        {
+            arr[s[i]]=i;
+        }
+        
+        vector<int>v;
+        int maxi=0;
+        for(int i=0;i<s.size();)
+        {
+            int j=i;
+            int maxi=arr[s[j]];
+            int ctr=0;
+            while(j<maxi)
+            {
+                ctr++;
+                if(arr[s[j]]>maxi)
+                {
+                    maxi=arr[s[j]];
+                }
+                j++;
+            }
+            v.push_back(ctr+1);
+            i=j+1;
+        }
+        return v;
+    }
+};
