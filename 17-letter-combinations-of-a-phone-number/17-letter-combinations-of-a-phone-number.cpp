be7011f31538dcdeb7aba3dc07d1c17ab76b84f7
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-03-30 23:49:01 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (35.40%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     
-    void solve(map<int,string>&m,string &ans,vector<string>&ds,string digits,int in)
+    void solve(string digits,map<int,string>&m,vector<string>&v,string &s,int in)
     {
         if(in==digits.size())
         {
-            if(ans.size()>0)
-            {
-                ds.push_back(ans);
-            }
+            v.push_back(s);
             return;
         }
-        string k=m[digits[in]-'0'];
-        for(int i=0;i<k.size();i++)
+        int a=digits[in]-'0';
+        for(int i=0;i<m[a].size();i++)
         {
-            ans+=k[i];
-            solve(m,ans,ds,digits,in+1);
-            ans.pop_back();
+            s+=m[a][i];
+            solve(digits,m,v,s,in+1);
+            s.pop_back();
         }
     }
+    
     
     vector<string> letterCombinations(string digits) {
         
         map<int,string>m;
-        m[2]="abc";
-        m[3]="def";
-        m[4]="ghi";
-        m[5]="jkl";
-        m[6]="mno";
-        m[7]="pqrs";
-        m[8]="tuv";
-        m[9]="wxyz";
+        m[2]="abc",m[3]="def",m[4]="ghi",m[5]="jkl",m[6]="mno",m[7]="pqrs",m[8]="tuv",m[9]="wxyz";
         
-        string ans="";
-        vector<string>ds;
+        if(digits.size()==0)
+            return {};
+        vector<string>v;
+        string s="";
         int in=0;
-        solve(m,ans,ds,digits,in);
-        return ds;
+        
+        solve(digits,m,v,s,in);
+        return v;
     }
 };
