65fff7a47fc5509a1a00f26cb0461fd26796c793
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-06-18 22:41:22 +0530

    Time: 11 ms (26.38%), Space: 7.9 MB (76.27%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     static bool cmp(vector<int>&a,vector<int>&b){
-       return (a[0]-a[1])<(b[0]-b[1]);
+       return (a[0]-a[1])>(b[0]-b[1]);
     }
     
     int twoCitySchedCost(vector<vector<int>>& costs) {
