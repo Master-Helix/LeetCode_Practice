af58f7871f70460bfc43fa6cdf8aa2ec9d82fc17
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-06-18 19:28:42 +0530

    Time: 238 ms (30.26%), Space: 73.1 MB (23.76%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    //Memoization Solution
+    //Tabulation Solution
     
     int solve(int i1,int i2,string &s1,string &s2,vector<vector<int>>&dp){
         if(i1<0 || i2<0){
@@ -24,8 +24,24 @@ public:
         reverse(s1.begin(),s1.end());
         int m=s.size();
         
-        vector<vector<int>>dp(m,vector<int>(m,-1));
+        vector<vector<int>>dp(m+1,vector<int>(m+1,0));
         
-        return solve(m-1,m-1,s,s1,dp);
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+            dp[0][i]=0;
+        }
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==s1[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][m];
+        
     }
 };
