88df42eb010664248d10f52d0fbb1c3a1fd6f431
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-06-20 00:40:22 +0530

    Added solution - LeetHub

---
+++ Alternate positive and negative numbers - GFG/alternate-positive-and-negative-numbers.cpp
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	   vector<int>pos;
+	   vector<int>neg;
+	   for(int i=0;i<n;i++){
+	       if(arr[i]>=0){
+	           pos.push_back(arr[i]);
+	       }
+	       else{
+	           neg.push_back(arr[i]);
+	       }
+	   }
+	   
+	   int i=0,j=0;
+	   int k=0;
+	   int flag=0;
+	   while(i<pos.size() && j<neg.size()){
+	       if(flag==0){
+	           arr[k]=pos[i];
+	           i++,k++;
+	           flag=1;
+	       }
+	       else{
+	           arr[k]=neg[j];
+	           j++,k++;
+	           flag=0;
+	       }
+	   }
+	   
+	   while(i<pos.size()){
+	       arr[k++]=pos[i++];
+	   }
+	   
+	   while(j<neg.size()){
+	       arr[k++]=neg[j++];
+	   }
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
