6234af74aeef6665de0b13e84e2f7d24f916f507
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-06-30 18:52:37 +0530

    Time: 17 ms (49.26%), Space: 10.8 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    
-         int minMoves2(vector<int>& nums) {
+    int minMoves2(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        if(n%2!=0)
-        {
+        int ans=0;
+        if(n%2!=0){
             int k=nums[n/2];
-            int sum=0;
-            for(int i=0;i<nums.size();i++)
-            {
-                sum+=abs(nums[i]-k);
+            for(int i=0;i<n;i++){
+                ans+=abs(k-nums[i]);
             }
-            return sum;
         }
-        else
-        {
-            int k1=nums[n/2];
-            int k2=nums[(n/2)-1];
-            int sum1=0,sum2=0;
-             for(int i=0;i<nums.size();i++)
-            {
-                sum1+=abs(nums[i]-k1);
-                 sum2+=abs(nums[i]-k2);
+        else{
+            int k1=nums[n/2],k2=nums[(n/2)-1];
+            int a1=0,a2=0;
+            for(int i=0;i<n;i++){
+                a1+=abs(k1-nums[i]);
+                a2+=abs(k2-nums[i]);
             }
-            return min(sum1,sum2);
+            ans=min(a1,a2);
         }
-        return 0;
+        return ans;
     }
-    
 };
