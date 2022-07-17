9bb58ac440461b5214365f35d65adeb7259844ca
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-07-17 19:49:30 +0530

    Time: 0 ms (100.00%), Space: 8 MB (18.56%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    int maximumNonAdjacentSum(vector<int> &nums){
-
-    int n=nums.size();
-    vector<int>dp(n,0);
-    dp[0]=nums[0];
-    for(int i=1;i<n;i++){
-        int left=nums[i];
-        int right;
-        if(i>1){
-            left=nums[i]+dp[i-2];
+    
+    int solve(vector<int>&t){
+        int n=t.size();
+        vector<int>dp(n,0);
+        dp[0]=t[0];
+        for(int i=1;i<n;i++){
+            int a=dp[i-1];
+            int b=t[i];
+            if(i>1){
+                b=t[i]+dp[i-2];
+            }
+            dp[i]=max(a,b);
         }
-        right=dp[i-1];
-        dp[i]=max(left,right);
+        return dp[n-1];
     }
-    return dp[n-1];
-}
-
-    int rob(vector<int>& value) {
+    
+    
+    int rob(vector<int>& nums) {
         vector<int>temp1,temp2;
-    int n=value.size();
-    
-    // if only one elemet , so no adjacent
-    
-    if(n==1){
-        return value[0];
-    }
-    
-    for(int i=0;i<n;i++){
-        if(i!=0){
-            temp1.push_back(value[i]);
+        if(nums.size()<2){
+            return nums[0];
         }
-        if(i!=n-1){
-            temp2.push_back(value[i]);
+        for(int i=0;i<nums.size();i++){
+            if(i!=0){
+                temp1.push_back(nums[i]);
+            }
+            if(i!=nums.size()-1){
+                temp2.push_back(nums[i]);
+            }
         }
-    }
-    return max(maximumNonAdjacentSum(temp1),maximumNonAdjacentSum(temp2));
+     
+        int k1=solve(temp1);
+        int k2=solve(temp2);
+        return max(k1,k2);
     }
 };
