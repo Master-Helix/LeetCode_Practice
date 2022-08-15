bae116b10d297c69e571326ab7769e31b3ba6474
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-08-15 15:42:50 +0530

    Time: 35 ms (16.87%), Space: 13 MB (5.10%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+        map<string,int>m;
+        m["I"]=1;
+        m["IV"]=4;
+        m["V"]=5;
+        m["IX"]=9;
+        m["X"]=10;
+        m["XL"]=40;
+        m["L"]=50;
+        m["XC"]=90;
+        m["C"]=100;
+        m["CD"]=400;
+        m["D"]=500;
+        m["CM"]=900;
+        m["M"]=1000;
+        
+        
+       int result=0;
+        
+        int i=0;
+        while(i<s.size())
+        {
+            string k="";
+            string k2="";
+            k+=s[i];
+            k+=s[i+1];
+            k2+=s[i];
+            if(m.find(k)!=m.end())
+            {
+                result+=m[k];
+                i+=2;
+            }
+            else
+            {
+                result+=m[k2];
+                i++;
+            }
+        }
+        if(i!=s.size())
+        {
+            string k2="";
+            k2+=s[i];
+            result+=m[k2];
+        }
+        return result;
+    }
+};
