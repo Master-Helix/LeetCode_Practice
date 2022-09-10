cca082acc9e96fdffcc7a03217f0eab7e2046a1a
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-09-10 21:50:24 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (39.40%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,17 +11,33 @@
  */
 class Solution {
 public:
-    
-    void solve(TreeNode *root, vector<int>&ans){
-        if(!root) return;
-        solve(root->left,ans);
-        ans.push_back(root->val);
-        solve(root->right,ans);
-    }
-    
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        solve(root,ans);
-        return ans;
+        
+        // morris traversal
+        
+        vector<int>inorder;
+        TreeNode *curr=root;
+        while(curr){
+            if(!curr->left){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode *prev=curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
