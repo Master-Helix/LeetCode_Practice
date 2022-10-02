96ec4d0af605c3ee52f80f59dc6d7cf3bffdaa96
Rakshit Pandey <87581340+Master-Helix@users.noreply.github.com>
2022-10-02 18:14:51 +0530

    Time: 15 ms (76.27%), Space: 8.8 MB (70.48%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        
-        int m=word1.size(),n=word2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-        
-        for(int i=0;i<=m;i++){
-            dp[i][0]=i;
-        }
-        for(int j=0;j<=n;j++){
-            dp[0][j]=j;
-        }
-        
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else{
-                    int a=1+dp[i-1][j];
-                    int b=1+dp[i][j-1];
-                    int c=1+dp[i-1][j-1];
-                    dp[i][j]=min(a,min(b,c));
-                }
-            }
-        }
-        return dp[m][n];
+    
+//Memoization Solution
+
+int solve(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
+    if(i<0){
+        return j+1;
     }
+    if(j<0){
+        return i+1;
+    }
+    
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    
+    if(s1[i]==s2[j]){
+        return dp[i][j]=solve(i-1,j-1,s1,s2,dp);
+    }
+    int a=1+solve(i-1,j,s1,s2,dp);
+        int b=1+solve(i-1,j-1,s1,s2,dp);
+        int c=1+solve(i,j-1,s1,s2,dp);
+        return dp[i][j]=min(a,min(b,c));
+}
+
+
+int minDistance(string str1, string str2)
+{
+   int m=str1.size(),n=str2.size();
+    vector<vector<int>>dp(m,vector<int>(n,-1));
+    return solve(m-1,n-1,str1,str2,dp);
+}
+  
 };
